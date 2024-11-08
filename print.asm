16c7cd45b7821d585e549c86faf008303dabfd34
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2024-11-09 02:25:15 +0600

    added

---
+++ print.asm
@@ -0,0 +1,27 @@
+
+
+
+
+;TITLE PGM4_2: PRINT STRING PROGRAM   
+
+
+.MODEL SMALL 
+.STACK 100H
+.DATA
+
+ 
+MSG DB 'HELLO!$'
+.CODE 
+MAIN PROC
+    ;initialize DS
+        MOV AX, @DATA
+        MOV DS, AX          ; initialize DS
+    ;display message
+        LEA DX, MSG         ;get message
+        MOV AH, 9           ;disply string function
+        INT 21h             ;display message
+    ;return to DOS
+        MOV AH, 4CH
+        INT 21h             ;DOS exit
+MAIN ENDP
+    END MAIN
