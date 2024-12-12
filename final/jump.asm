744aedaa2748e51647d315d8e7f1aef53a348d4b
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2024-12-12 14:53:15 +0600

    modified jmp.asm

--- final/jump.asm
+++ final/jump.asm
@@ -4,7 +4,7 @@
 MOV AX, 1234H;
 MOV BX, 3456H;
 
-
+; If jump is comment out then the program will go in a loop state after executing the last line
 JMP DEF
 
 MOV CX, 3456H;
@@ -14,6 +14,7 @@ JKL: MOV DX, 5678;
 
 DEF: MOV DX, 5678H;
 
+;Reusing the same 15 line would cause error
 ;DEF: MOV DX, 5678H;    
 
 
