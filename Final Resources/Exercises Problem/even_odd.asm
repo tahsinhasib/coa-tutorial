1245057eaaabf3a6636c61fba1bc345d3b5ad1f8
Tahsin Hasib <tahsinhasib@gmail.com>
2025-01-05 01:45:16 +0600

    added even odd using case

---
+++ Final Resources/Exercises Problem/even_odd.asm
@@ -0,0 +1,48 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+
+.CODE
+    MAIN PROC
+        ; Initialize Data Segment
+        MOV AX, @DATA
+        MOV DS, AX
+
+        ; Read a character from keyboard
+        MOV AH, 1
+        INT 21H
+
+        ; Compare AL with ASCII codes of '1', '2', '3', and '4'
+        CMP AL, '1'
+        JE ODD  
+        CMP AL, '3'
+        JE ODD
+        CMP AL, '2'
+        JE EVEN
+        CMP AL, '4'
+        JE EVEN
+    
+        JMP END_CASE
+    
+        ODD:
+            MOV AH, 2
+            MOV DL, 'O'
+            JMP DISPLAY
+    
+        EVEN:
+            MOV AH, 2
+            MOV DL, 'E'
+            JMP DISPLAY
+        
+        DISPLAY:
+            INT 21H
+            JMP END_CASE
+
+        ; Exit program
+        END_CASE:
+            MOV AH, 4CH
+            INT 21H
+
+    MAIN ENDP
+END MAIN
