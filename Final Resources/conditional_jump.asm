cec5f4b740ffaf2f725534280ad185686bd4a2a5
Tahsin Hasib <99963332+tahsinhasib@users.noreply.github.com>
2025-01-06 22:27:46 +0600

    added unconditional code from some source which contains bug

---
+++ Final Resources/conditional_jump.asm
@@ -0,0 +1,52 @@
+
+.model small
+.stack 100h
+
+.data
+    hello db 0dh, "Hello World$"
+    bye db 0dh, "Bye World$"
+    count db 5
+
+.code
+    mov ax, @data
+    mov ds, ax
+
+    ; Initialize loop counter
+    mov cx, 5
+
+print_hello:
+    mov ah, 09h     ; DOS function to print a string
+    lea dx, hello   ; Load address of hello string
+    int 21h         ; Call DOS 
+    
+     ; Print a newline character
+    mov ah, 02h     ; DOS function to print a character
+    mov dl, 0Ah     ; ASCII code for newline
+    int 21h         ; Call DOS
+
+   ;Decremen the counter  
+    dec cx;
+
+    ; Check if we've printed "Hello World" five times
+    CMP cx,0;
+    JG  print_hello:
+   
+    JNE print_bye    ; If count is zero, jump to print_bye
+
+  
+print_bye:
+    ; Print a newline character
+    mov ah, 02h     ; DOS function to print a character
+    mov dl, 0Ah     ; ASCII code for newline
+    int 21h         ; Call DOS
+
+    ; Print "Bye World"
+    mov ah, 09h     ; DOS function to print a string
+    lea dx, bye     ; Load address of bye string
+    int 21h         ; Call DOS
+
+    ; Exit the program
+    mov ah, 4Ch     ; DOS function to exit
+    int 21h         ; Call DOS
+
+end
